8b94323542f327383338c027fb3091c7eac358d4
borislavvp <46525030+borislavvp@users.noreply.github.com>
2023-12-27 02:03:25 +0800

    changed preprocessing
    extract keypoints from superglue and loftr
    concatenate
    crop
    build superlist

--- src/preprocessing/crop.py
+++ src/preprocessing/crop.py
@@ -1,10 +1,13 @@
+import pickle
 import cv2
 import numpy as np
+import os
+import torch
 from tqdm import tqdm
 from pathlib import Path
-from src.external.superglue.models.matching import Matching
-from src.external.superglue.models.utils import read_image
 
+from src.external.superglue.models.utils import read_image
+from .pairs import get_pairs
 from sklearn.cluster import DBSCAN
 
 def crop_image(image, matching_points, eps, min_samples=5):
@@ -30,89 +33,29 @@ def crop_image(image, matching_points, eps, min_samples=5):
     max_y = int(np.max(cluster_points[:, 1]))
     # Crop the image based on the cluster coordinates
     cropped_image = image[int(min_y):int(max_y), int(min_x):int(max_x)]
-
+    
     return cropped_image,min_x,min_y
 
-def get_pairs(path):
-    with open(path, "r") as f:
-        # initialize an empty list to store the pairs
-        pairs = []
-        # loop through each line of the file
-        for line in f:
-            # split the line by whitespace and assign to a list
-            pair = line.split()
-            # append the list to another list
-            pairs.append(pair)
-
-        # print the list of pairs
-        return pairs
-    
-def extract_features_superglue(config, inp0,inp1,device):
-    matching = Matching(config).eval().to(device)
-
-    # Perform the matching.
-    pred = matching({'image0': inp0, 'image1': inp1})
-    pred = {k: v[0].cpu().detach().numpy() for k, v in pred.items()}
-    kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
-    matches, conf = pred['matches0'], pred['matching_scores0']
-
-    valid = matches > 10
-    mkpts0 = kpts0[valid]
-    mkpts1 = kpts1[matches[valid]]
-
-    return mkpts0, mkpts1,kpts0,kpts1,conf,valid, matches
-
-def scale_to_resized(mkpts0, mkpts1, scale1,scale2):
-    ### scale to original im size because we used max_image_size
-    # first point
-    mkpts0[:, 0] = mkpts0[:, 0] / scale1[0]
-    mkpts0[:, 1] = mkpts0[:, 1] / scale1[1]    
-    # second point
-    mkpts1[:, 0] = mkpts1[:, 0] / scale2[0]
-    mkpts1[:, 1] = mkpts1[:, 1] / scale2[1]
-    
-    return mkpts0, mkpts1
-
-def crop_images(images_dir: Path,pairs_path,output_dir):
+def crop_images(images_dir: Path,pairs_path,output_dir,keypoints,resize = [1376,]):
     matches_per_image = {}
 
-    device =  'cpu'
-    resize = [1600, ]
-    resize_float = True
-
     pairs = get_pairs(pairs_path)
+ 
 
-    config = {
-            "superpoint": {
-                "nms_radius": 4,
-                "keypoint_threshold": 0.005,
-                "max_keypoints": 1024
-            },
-            "superglue": {
-                "weights": "outdoor",
-                "sinkhorn_iterations": 20,
-                "match_threshold": 0.2,
-            }
-        }
-    
     for image_0_name,image_1_name in tqdm(pairs, desc=f"Cropping {images_dir.name}", ncols=80):
         img0_path = images_dir / image_0_name
         
         img1_path = images_dir / image_1_name
-        
-        image0, inp0, scales0 = read_image(img0_path,device,resize,0,resize_float)
-        image1, inp1, scales1 = read_image(img1_path,device,resize,0,resize_float)
 
-        matches0, matches1,kpts0,kpts1,conf,valid, matches = extract_features_superglue(config,inp0,inp1,device)
-        
-        # mkpts11,mkpts22 = scale_to_resized(matches0, matches1, scales0,scales1)
-        # mkpts1=mkpts11
-        # mkpts2=mkpts22
-        
-        if( image_0_name not in matches_per_image or len(matches0) > matches_per_image[image_0_name]):
-            print(f"Biggest matches found: {len(matches0)}")
-            matches_per_image[image_0_name] = len(matches0)
+        image0, inp0, scales0 = read_image(img0_path,"cpu",resize,0,True)
+        image1, inp1, scales1 = read_image(img1_path,"cpu",resize,0,True)
+
+        current = keypoints[image_0_name][image_1_name]["keypoints0"]
+
+        if( image_0_name not in matches_per_image or len(current) > matches_per_image[image_0_name]):
+            print(f"Biggest matches found: {len(current)}")
+            matches_per_image[image_0_name] = len(current)
 
             print(f"Crop image: {image_0_name}")
-            crop_image0,_,_ = crop_image(image0, matches0, 20)
-            cv2.imwrite(str(output_dir / image_0_name),crop_image0 )
+            crop_image0,_,_ = crop_image(image0, current, 20)
+            cv2.imwrite(str(output_dir / image_0_name), crop_image0)
--- src/preprocessing/main.py
+++ src/preprocessing/main.py
@@ -1,11 +1,67 @@
 import argparse
 import os
+import pickle
 
 from typing import Any, Dict, Tuple
+
+import numpy as np
 from src.config.paths import DataPaths
 from .rotate import rotate_images 
 from .pairs import compute_pairs 
 from .crop import crop_images 
+from src.matching.loftr import loftr
+from src.matching.superglue import superglue
+
+def concat_keypoints(keypoints1_pickle,keypoints2_pickle):
+    keypoints1 = None
+    keypoints2 = None
+
+    if(keypoints1_pickle):
+        with open(keypoints1_pickle, "rb") as file:
+            keypoints1 = pickle.load(file)
+
+    if(keypoints2_pickle):
+        with open(keypoints2_pickle, "rb") as file:
+            keypoints2 = pickle.load(file)
+
+
+    concatenated = {}
+
+    for image,v in keypoints1:
+        for image_pair,pair_keypoints in v:
+            kp0 = pair_keypoints["keypoints0"]
+            kp1 = pair_keypoints["keypoints0"]
+
+            kp00 = keypoints2[image][image_pair]["keypoints0"]
+            kp11 = keypoints2[image][image_pair]["keypoints0"]
+
+            concatenated[image][image_pair]["keypoints0"] = np.vstack((kp0,kp00)) 
+            concatenated[image][image_pair]["keypoints1"] = np.vstack((kp1,kp11)) 
+    
+    return concatenated
+
+def build_superlist(keypoints):
+    superlist = {} 
+    temp_counter = {}
+
+    def get_super_pair(latest_super_pair):
+        super_pair = None
+        for image_pair,pair_keypoints in keypoints[latest_super_pair].items():
+            kp = pair_keypoints["keypoints0"]
+            if(image_pair not in superlist and (latest_super_pair not in superlist or len(kp) > temp_counter[latest_super_pair])):
+                temp_counter[latest_super_pair] = len(kp)
+                superlist[latest_super_pair] = image_pair
+                super_pair = image_pair
+        return super_pair
+    
+    latest_super_pair = list(keypoints.keys())[0]
+
+    while(latest_super_pair is not None):
+        next_super_pair = get_super_pair(latest_super_pair)
+        superlist[latest_super_pair] = next_super_pair
+        latest_super_pair = next_super_pair
+
+    return superlist
 
 def preprocess(
     paths: DataPaths,
@@ -26,6 +82,16 @@ def preprocess(
     # compute pairs 
     compute_pairs(paths.rotated_image_dir, image_list, paths.features_retrieval, paths.pairs_path)
 
-    # crop images 
-    crop_images(paths.rotated_image_dir, paths.pairs_path, paths.cropped_image_dir)
+    # TODO: run in parallel
+    # extract important keypoints 
+    superglue(paths.rotated_image_dir,paths.pairs_path, paths.superglue_keypoints_pickle)
+    loftr(paths.rotated_image_dir,paths.pairs_path, paths.superglue_keypoints_pickle)
+    
+    # concat important keypoints
+    keypoints = concat_keypoints(paths.superglue_keypoints_pickle,paths.loftr_keypoints_pickle)
 
+    # crop images 
+    crop_images(paths.rotated_image_dir, paths.pairs_path, paths.cropped_image_dir,keypoints)
+
+    # build superlist
+    superlist = build_superlist(keypoints)
