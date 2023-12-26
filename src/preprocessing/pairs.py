d44584c80dd78dbcbc3a68a2a8dfa1e46fb00299
borislavvp <46525030+borislavvp@users.noreply.github.com>
2023-12-27 02:01:34 +0800

    use eigenplaces instead of netvlad for pairs retrieval

--- src/preprocessing/pairs.py
+++ src/preprocessing/pairs.py
@@ -6,11 +6,25 @@ from src.external.hloc import (
 )
 from pathlib import Path
 
+def get_pairs(path):
+    with open(path, "r") as f:
+        # initialize an empty list to store the pairs
+        pairs = []
+        # loop through each line of the file
+        for line in f:
+            # split the line by whitespace and assign to a list
+            pair = line.split()
+            # append the list to another list
+            pairs.append(pair)
+
+        # print the list of pairs
+        return pairs
+    
 def compute_pairs(image_dir:Path,img_list,features_retrieval_path,pairs_path) -> None: 
     print("Compute pairs")
 
     extract_features.main(
-        conf=extract_features.confs["netvlad"],
+        conf=extract_features.confs["eigenplaces"],
         image_dir=image_dir,
         image_list=img_list,
         feature_path=features_retrieval_path,
