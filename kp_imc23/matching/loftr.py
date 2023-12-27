f2160fcb420a73ae6140995d1b818e8fe1857b5e
borislavvp <46525030+borislavvp@users.noreply.github.com>
2023-12-27 18:29:32 +0800

    added feature extraction and matching by splitting images into regions

--- kp_imc23/matching/loftr.py
+++ kp_imc23/matching/loftr.py
@@ -1,28 +1,38 @@
 import pickle
+from matplotlib import cm
 import numpy as np
 from tqdm import tqdm
 from pathlib import Path
 
 from kp_imc23.preprocessing.pairs import get_pairs
 from kp_imc23.external.superglue.models.matching import Matching
-from kp_imc23.external.superglue.models.utils import read_image, get_torch_device
+from kp_imc23.external.superglue.models.utils import frame2tensor, make_matching_plot, read_image, get_torch_device
 import torch
 import kornia as K
 import kornia.feature as KF
 
-def extract_features_superglue(matching, config, inp0,inp1,device):
-    # Perform the matching.
-    pred = matching({'image0': inp0, 'image1': inp1})
-    pred = {k: v[0].cpu().detach().numpy() for k, v in pred.items()}
-    kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
-    matches, conf = pred['matches0'], pred['matching_scores0']
+from kp_imc23.preprocessing.utils import split_images_into_regions
 
-    valid = matches > 10
-    mkpts0 = kpts0[valid]
-    mkpts1 = kpts1[matches[valid]]
+def make_plot(image0, image1, kpts0, kpts1, mkpts0, mkpts1, conf):
+    alpha = 0
+    color = cm.jet(conf, alpha=alpha)
 
-    return mkpts0, mkpts1, conf,valid, matches
-
+    conf_min = conf.min()
+    conf_max = conf.max()
+    text = [
+        f'LoFTR',
+        '# Matches (showing/total): {}/{}'.format(len(mkpts0), len(mkpts0)),
+    ]
+    small_text = [
+        f'Showing matches from {0}:{2000}',
+        # f'Confidence Range: {conf_min:.2f}:{conf_max:.2f}',
+        # 'Image Pair: {:06}:{:06}'.format(stem0, stem1),
+    ]
+    # Make the matching plot and save it to a file
+    make_matching_plot(
+        image0, image1, mkpts0, mkpts1, mkpts0, mkpts1, color,
+        text, Path("./result.png"), True, 'Matches', small_text)
+        
 def scale_to_resized(mkpts0, mkpts1, scale1,scale2):
     ### scale to original im size because we used max_image_size
     # first point
@@ -34,16 +44,66 @@ def scale_to_resized(mkpts0, mkpts1, scale1,scale2):
     
     return mkpts0, mkpts1
 
-def loftr(images_dir: Path,pairs_path,weights_path,output_dir,resize = [1376,]):
-    device =  get_torch_device()
-    # device = torch.device('cuda')
-    # resize = [[840,], [1024,], [1280,] ]
-
-    pairs = get_pairs(pairs_path)
+def get_model(weights_path,device):
 
     matcher = KF.LoFTR(pretrained=None)
     matcher.load_state_dict(torch.load(weights_path)['state_dict'])
     matcher = matcher.to(device).eval()
+    return matcher
+
+def extract_features(model,inp0,inp1):
+    with torch.inference_mode():
+        input_dict = {"image0": inp0,"image1": inp1}
+        correspondences = model(input_dict)
+
+    mkpts0 = correspondences['keypoints0'].cpu().numpy()
+    mkpts1 = correspondences['keypoints1'].cpu().numpy()
+    conf = correspondences['confidence'].cpu().numpy()
+    
+    return mkpts0, mkpts1, conf
+
+def loft_split_matching(model, image0, image1):
+    # Split the images into regions using the specified method
+    tiles1, tiles2, offsets = split_images_into_regions(image0, image1)
+    # Initialize an empty list for storing the matches
+    matches = []
+
+    # Loop through the regions and match them using SuperGlue
+        
+    for i, (tile1, tile2) in enumerate(zip(tiles1, tiles2)):
+        # Convert the images to tensors
+        inp0 = frame2tensor(tile1, "cuda")
+        inp1 = frame2tensor(tile2, "cuda")
+        
+        mkpts0, mkpts1, conf = extract_features(model,inp0,inp1)
+        
+        # Add the offset to the coordinates of the keypoints and matches
+        x_offset, y_offset = offsets[i]
+        # make_plot(tile1, tile2, mkpts0, mkpts1, mkpts0, mkpts1, conf, True)
+        mkpts0[:, 0] += x_offset
+        mkpts0[:, 1] += y_offset
+        mkpts1[:, 0] += x_offset
+        mkpts1[:, 1] += y_offset
+        # Append the matches to the list
+        matches.append(( mkpts0, mkpts1, conf))
+
+    # Concatenate the matches from all regions
+    mkpts0, mkpts1, conf = zip(*matches)
+    mkpts0 = np.concatenate(mkpts0, axis=0)
+    mkpts1 = np.concatenate(mkpts1, axis=0)
+    conf = np.concatenate(conf, axis=0)
+
+    # Make the final plot of the matched images
+    # make_plot(image0, image1, kpts0, kpts1, mkpts0, mkpts1, conf, True)
+    return mkpts0, mkpts1, conf
+
+
+def loftr(images_dir: Path,pairs_path,weights_path,output_dir,resize = [1376,], with_splitting = False):
+    device =  get_torch_device()
+
+    pairs = get_pairs(pairs_path)
+
+    model = get_model(weights_path, device)
 
     keypoints = {}
 
@@ -60,17 +120,15 @@ def loftr(images_dir: Path,pairs_path,weights_path,output_dir,resize = [1376,]):
       
         image0, inp0, scales0 = read_image(img0_path,device,resize,0,True)
         image1, inp1, scales1 = read_image(img1_path,device,resize,0,True)
+    
+        mkpts0, mkpts1 = {}
 
-        with torch.inference_mode():
-            input_dict = {"image0": inp0,"image1": inp1}
-            correspondences = matcher(input_dict)
-
-        mkpts0 = correspondences['keypoints0'].cpu().numpy()
-        mkpts1 = correspondences['keypoints1'].cpu().numpy()
-        
+        if(with_splitting):
+            mkpts0, mkpts1 = loft_split_matching(model,image0,image1)
+        else:
+            mkpts0, mkpts1 = extract_features(model,inp0,inp1)
         # mkpts0, mkpts1 = scale_to_resized(mkpts0,mkpts1,scales0,scales1)
 
-        
         keypoints[image_0_name][image_1_name] = {"keypoints0":mkpts0,"keypoints1":mkpts1}
 
     with open(output_dir, "wb") as file:
--- kp_imc23/matching/superglue.py
+++ kp_imc23/matching/superglue.py
@@ -1,43 +1,16 @@
 import pickle
+from matplotlib import cm
 import numpy as np
 from tqdm import tqdm
 from pathlib import Path
 from kp_imc23.preprocessing.pairs import get_pairs
 from kp_imc23.external.superglue.models.matching import Matching
-from kp_imc23.external.superglue.models.utils import read_image, get_torch_device
+from kp_imc23.external.superglue.models.utils import frame2tensor, make_matching_plot, read_image, get_torch_device
+from kp_imc23.preprocessing.utils import split_images_into_regions
 
 
-def extract_features_superglue(matching, config, inp0,inp1,device):
-    # Perform the matching.
-    pred = matching({'image0': inp0, 'image1': inp1})
-    pred = {k: v[0].cpu().detach().numpy() for k, v in pred.items()}
-    kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
-    matches, conf = pred['matches0'], pred['matching_scores0']
-
-    valid = matches > 10
-    mkpts0 = kpts0[valid]
-    mkpts1 = kpts1[matches[valid]]
-
-    return mkpts0, mkpts1, conf,valid, matches
-
-def scale_to_resized(mkpts0, mkpts1, scale1,scale2):
-    ### scale to original im size because we used max_image_size
-    # first point
-    mkpts0[:, 0] = mkpts0[:, 0] / scale1[0]
-    mkpts0[:, 1] = mkpts0[:, 1] / scale1[1]    
-    # second point
-    mkpts1[:, 0] = mkpts1[:, 0] / scale2[0]
-    mkpts1[:, 1] = mkpts1[:, 1] / scale2[1]
+def get_model(device):
     
-    return mkpts0, mkpts1
-
-def superglue(images_dir: Path,pairs_path,output_dir, resize = [1376,]):
-    device = get_torch_device()
-    # device = torch.device('cuda')
-    # resize = [[840,], [1024,], [1280,] ]
-
-    pairs = get_pairs(pairs_path)
-
     config = {
         "superpoint": {
             "nms_radius": 4,
@@ -52,6 +25,107 @@ def superglue(images_dir: Path,pairs_path,output_dir, resize = [1376,]):
     }
       
     matching = Matching(config).eval().to(device)
+    return matching
+
+def scale_to_resized(mkpts0, mkpts1, scale1,scale2):
+    ### scale to original im size because we used max_image_size
+    # first point
+    mkpts0[:, 0] = mkpts0[:, 0] / scale1[0]
+    mkpts0[:, 1] = mkpts0[:, 1] / scale1[1]    
+    # second point
+    mkpts1[:, 0] = mkpts1[:, 0] / scale2[0]
+    mkpts1[:, 1] = mkpts1[:, 1] / scale2[1]
+    
+    return mkpts0, mkpts1
+
+
+def make_plot(model, image0, image1, kpts0, kpts1, mkpts0, mkpts1, conf, valid):
+    # Get the color map for the matches
+    color = cm.jet(conf[valid])
+    # # Define the text for the plot
+    text = [
+        'SuperGlue',
+        'Keypoints: {}:{}'.format(len(kpts0), len(kpts1)),
+        'Matches: {}'.format(len(mkpts0)),
+    ]
+
+    # Display extra parameter info.
+    k_thresh = model.superpoint.config['keypoint_threshold']
+    m_thresh = model.superglue.config['match_threshold']
+    small_text = [
+        'Keypoint Threshold: {:.4f}'.format(k_thresh),
+        'Match Threshold: {:.2f}'.format(m_thresh),
+    ]
+
+    make_matching_plot(
+        image0, image1, mkpts0, mkpts1, mkpts0, mkpts1, color,
+        text, Path("./result.png"), True, 'Matches', small_text)
+        
+
+def extract_features(model, inp0,inp1):
+    # Perform the model.
+    pred = model({'image0': inp0, 'image1': inp1})
+    pred = {k: v[0].cpu().detach().numpy() for k, v in pred.items()}
+    kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
+    matches, conf = pred['matches0'], pred['matching_scores0']
+
+    valid = matches > 10
+    mkpts0 = kpts0[valid]
+    mkpts1 = kpts1[matches[valid]]
+
+    return mkpts0, mkpts1, conf,valid, matches, kpts0, kpts1
+
+def extract_features_split_matching(model, image0,image1):
+    # Split the images into regions using the specified method
+    tiles1, tiles2, offsets = split_images_into_regions(image0, image1)
+    # Get the SuperGlue model with the specified type
+    # Initialize an empty list for storing the matches
+    matches = []
+
+    # Loop through the regions and match them using SuperGlue
+        
+    for i, (tile1, tile2) in enumerate(zip(tiles1, tiles2)):
+        # Convert the images to tensors
+        inp1 = frame2tensor(tile1, "cuda")
+        inp2 = frame2tensor(tile2, "cuda")
+
+        # Extract the features and matches using SuperGlue
+        mkpts0, mkpts1, conf, valid, _, kpts0, kpts1 = extract_features(model, inp1, inp2)
+        # Add the offset to the coordinates of the keypoints and matches
+        x_offset, y_offset = offsets[i]
+        # make_plot(tile1, tile2, mkpts0, mkpts1, mkpts0, mkpts1, conf, True)
+        kpts0[:, 0] += x_offset
+        kpts0[:, 1] += y_offset
+        kpts1[:, 0] += x_offset
+        kpts1[:, 1] += y_offset
+        mkpts0[:, 0] += x_offset
+        mkpts0[:, 1] += y_offset
+        mkpts1[:, 0] += x_offset
+        mkpts1[:, 1] += y_offset
+        # Append the matches to the list
+        matches.append((mkpts0, mkpts1, mkpts0, mkpts1, conf, valid))
+
+    # Concatenate the matches from all regions
+    kpts0, kpts1, mkpts0, mkpts1, conf, valid = zip(*matches)
+    kpts0 = np.concatenate(kpts0, axis=0)
+    kpts1 = np.concatenate(kpts1, axis=0)
+    mkpts0 = np.concatenate(mkpts0, axis=0)
+    mkpts1 = np.concatenate(mkpts1, axis=0)
+    conf = np.concatenate(conf, axis=0)
+    valid = np.concatenate(valid, axis=0)
+    # make_plot(image0, image1, kpts0, kpts1, mkpts0, mkpts1, conf, valid)
+
+    return mkpts0, mkpts1, conf, valid, matches, kpts0, kpts1
+    # Make the final plot of the matched images
+
+
+def superglue(images_dir: Path,pairs_path,output_dir, resize = [1376,],with_splitting = False):
+    device =  'cpu'
+    # device = torch.device('cuda')
+    # resize = [[840,], [1024,], [1280,] ]
+
+    pairs = get_pairs(pairs_path)
+    model = get_model(device)
     keypoints = {}
 
     for image_0_name,image_1_name in tqdm(pairs, desc=f"Superglue {images_dir.name}", ncols=80):
@@ -67,7 +141,12 @@ def superglue(images_dir: Path,pairs_path,output_dir, resize = [1376,]):
         image0, inp0, scales0 = read_image(img0_path,device,resize,0,True)
         image1, inp1, scales1 = read_image(img1_path,device,resize,0,True)
 
-        mkpts0, mkpts1, conf, valid, matches = extract_features_superglue(matching, config,inp0,inp1,device)
+        mkpts0, mkpts1 = {}
+
+        if(with_splitting):
+            mkpts0, mkpts1 = extract_features_split_matching(img0_path,img1_path)
+        else:
+            mkpts0, mkpts1 = extract_features(model,inp0,inp1)
         
         keypoints[image_0_name][image_1_name] = {"keypoints0":mkpts0,"keypoints1":mkpts1}
 
