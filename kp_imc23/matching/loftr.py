506860ef57682c9c359785548ce9419f16947110
Raphael 泰山 El Haddad <raphaelhaddad@Raphaels-MacBook-Air.local>
2023-12-27 22:22:06 +0800

    test

--- kp_imc23/matching/loftr.py
+++ kp_imc23/matching/loftr.py
@@ -123,9 +123,9 @@ def loftr(images_dir: Path,pairs_path,weights_path,output_dir,resize = [1376,], 
     
 
         if(with_splitting):
-            mkpts0, mkpts1 = loft_split_matching(model,image0,image1)
+            mkpts0, mkpts1, _ = loft_split_matching(model,image0,image1)
         else:
-            mkpts0, mkpts1 = extract_features(model,inp0,inp1)
+            mkpts0, mkpts1, _ = extract_features(model,inp0,inp1)
         # mkpts0, mkpts1 = scale_to_resized(mkpts0,mkpts1,scales0,scales1)
 
         keypoints[image_0_name][image_1_name] = {"keypoints0":mkpts0,"keypoints1":mkpts1}
