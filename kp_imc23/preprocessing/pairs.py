64f00982ea6e03ce31ee97dc963e4013a10ff1ea
borislavvp <46525030+borislavvp@users.noreply.github.com>
2023-12-29 11:49:26 +0800

    change pairs

--- kp_imc23/preprocessing/pairs.py
+++ kp_imc23/preprocessing/pairs.py
@@ -24,7 +24,7 @@ def compute_pairs(image_dir:Path,img_list,features_retrieval_path,pairs_path) ->
     print("Compute pairs")
 
     extract_features.main(
-        conf=extract_features.confs["eigenplaces"],
+        conf=extract_features.confs["netvlad"],
         image_dir=image_dir,
         image_list=img_list,
         feature_path=features_retrieval_path,
@@ -32,7 +32,7 @@ def compute_pairs(image_dir:Path,img_list,features_retrieval_path,pairs_path) ->
 
     pairs_from_retrieval.main(
         descriptors=features_retrieval_path,
-        num_matched=10,
+        num_matched=30,
         output=pairs_path,
     )   
 
