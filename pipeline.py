5c57af37cc6aaeef9fe41adb1e404a2a5ad90633
kevin.pruvost <pruvostkevin0@gmail.com>
2023-12-29 10:55:54 +0800

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -43,7 +43,7 @@ def configurate(data_dir, output_dir, dataset, scene, mode):
     paths = DataPaths(Path(data_dir), Path(output_dir), dataset, scene, mode)
     return paths
 
-def main(data_dir, dataset, scene, mode="train"):
+def main(data_dir, dataset, scene, mode="train", preprocess_matcher="lightglue"):
     # paths = configurate(
     #     data_dir=".",
     #     output_dir="./output",
@@ -62,7 +62,7 @@ def main(data_dir, dataset, scene, mode="train"):
     image_list = os.listdir(paths.input_dir_images)
 
     # preprocess images
-    preprocess(paths, image_list, args=None)
+    preprocess(paths, image_list, args=None, matcher=preprocess_matcher)
 
     # Database
     database_colmap_run(paths, image_list, args=None)
