e9b23e824ea04e5b09d7353eafa089f1a29af2a7
kevin.pruvost <pruvostkevin0@gmail.com>
2024-01-02 11:06:03 +0800

    Update main.py

--- kp_imc23/preprocessing/main.py
+++ kp_imc23/preprocessing/main.py
@@ -43,6 +43,7 @@ def preprocess(
     #print(f"test: {paths.split_image_dir}")
 
     # # compute pairs 
+    if len(image_list) > 100: num_pairs = min(num_pairs, 10)
     compute_pairs(paths.rotated_image_dir, image_list, paths.features_retrieval, paths.pairs_path, num_pairs=num_pairs)
     #print(f"test: {paths.split_image_dir}")
     # # # extract important keypoints 
